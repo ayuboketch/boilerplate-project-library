a16fa1c58d5c245f66193df10b6917e233c51729
Ayub Oketch <ayuboketch84@gmail.com>
2025-04-26 10:56:53 +0000

    Initial commit

--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,102 @@
-/*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
-
+// routes/api.js
 'use strict';
+const mongoose = require('mongoose');
 
-module.exports = function (app) {
+// Define Book schema and model
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  comments: [String]
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = function(app) {
+  // CONNECT TO DB
+  const db = process.env.DB;
+  mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
 
   app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+    .post(async (req, res) => {
+      const { title } = req.body;
+      if (!title) return res.send('missing required field title');
+      try {
+        const book = new Book({ title, comments: [] });
+        const saved = await book.save();
+        res.json({ _id: saved._id, title: saved.title });
+      } catch (err) {
+        res.status(500).send('server error');
+      }
     })
-    
-    .post(function (req, res){
-      let title = req.body.title;
-      //response will contain new book object including atleast _id and title
+
+    .get(async (req, res) => {
+      try {
+        const books = await Book.find({});
+        const formatted = books.map(b => ({
+          _id: b._id,
+          title: b.title,
+          commentcount: b.comments.length
+        }));
+        res.json(formatted);
+      } catch (err) {
+        res.status(500).send('server error');
+      }
     })
-    
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
+
+    .delete(async (req, res) => {
+      try {
+        await Book.deleteMany({});
+        res.send('complete delete successful');
+      } catch (err) {
+        res.status(500).send('server error');
+      }
     });
-
-
 
   app.route('/api/books/:id')
-    .get(function (req, res){
-      let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+    .get(async (req, res) => {
+      const { id } = req.params;
+      if (!mongoose.Types.ObjectId.isValid(id)) return res.send('no book exists');
+      try {
+        const book = await Book.findById(id);
+        if (!book) return res.send('no book exists');
+        res.json({
+          _id: book._id,
+          title: book.title,
+          comments: book.comments
+        });
+      } catch (err) {
+        res.status(500).send('server error');
+      }
     })
-    
-    .post(function(req, res){
-      let bookid = req.params.id;
-      let comment = req.body.comment;
-      //json res format same as .get
+
+    .post(async (req, res) => {
+      const { id } = req.params;
+      const { comment } = req.body;
+      if (!comment) return res.send('missing required field comment');
+      if (!mongoose.Types.ObjectId.isValid(id)) return res.send('no book exists');
+      try {
+        const book = await Book.findById(id);
+        if (!book) return res.send('no book exists');
+        book.comments.push(comment);
+        const updated = await book.save();
+        res.json({
+          _id: updated._id,
+          title: updated.title,
+          comments: updated.comments
+        });
+      } catch (err) {
+        res.status(500).send('server error');
+      }
     })
-    
-    .delete(function(req, res){
-      let bookid = req.params.id;
-      //if successful response will be 'delete successful'
+
+    .delete(async (req, res) => {
+      const { id } = req.params;
+      if (!mongoose.Types.ObjectId.isValid(id)) return res.send('no book exists');
+      try {
+        const deleted = await Book.findByIdAndDelete(id);
+        if (!deleted) return res.send('no book exists');
+        res.send('delete successful');
+      } catch (err) {
+        res.status(500).send('server error');
+      }
     });
-  
 };
